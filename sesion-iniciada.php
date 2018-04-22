b4489db1460679939701a296ed558ab32b73b73f
GioProyects <giogow3halo@gmail.com>
2018-04-22 13:22:09 -0500

    final

--- sesion-iniciada.php
+++ sesion-iniciada.php
@@ -11,8 +11,11 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
 
  <!DOCTYPE html>
  <html>
+ <html lang="es">
    <head>
      <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title></title>
    </head>
    <body>
