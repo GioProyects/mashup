8fd9e94afe976279445f0de2f29012b24083be69
GioProyects <giogow3halo@gmail.com>
2018-04-22 13:53:24 -0500

    asdf

--- php/sesion-iniciada.php
+++ php/sesion-iniciada.php
@@ -4,7 +4,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
 
 }else {
   echo "<p> Esta pagina es solo para usuarios registrados</p>";
-  echo "<br/><a href='index.html'> Iniciar Sesion</a>";
+  echo "<br/><a href='index.php'> Iniciar Sesion</a>";
   exit;
 }
  ?>
@@ -16,7 +16,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Ventana Principal</title>
+     <title>Mashup</title>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
      <link rel="stylesheet" href="css/estilos.css">
