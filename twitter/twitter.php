38c8498988639d231d220a978096ee0750474dea
GioProyects <giogow3halo@gmail.com>
2018-04-22 11:01:26 -0500

    sfsd

--- twitter/twitter.php
+++ twitter/twitter.php
@@ -2,10 +2,10 @@
 require_once 'TwitterAPIExchange.php';
 define('CONSUMER_KEY', 'IsfOhNHmYtQS5myPZvXB7kqCf');
 define('CONSUMER_SECRET', 'hSe6ZQbao5wEyIvlGFXhA1itlSME9NBhsqOsiKYm5jmOUFJLMx');
-// $TOKEN='449924072-LfvTLKWeVwVGKqDCoISSOrAUDVZx2tSaJjEN6aDe';
-// $TOKEN_SECRET='cXJLCOaSna7LycPH0TephGBQkYhwuv3h9lCdAqg8c96RU';
-$TOKEN=$_SESSION["oauth_token"];
-$TOKEN_SECRET=$_SESSION["oauth_token_secret"];
+$TOKEN='449924072-LfvTLKWeVwVGKqDCoISSOrAUDVZx2tSaJjEN6aDe';
+$TOKEN_SECRET='cXJLCOaSna7LycPH0TephGBQkYhwuv3h9lCdAqg8c96RU';
+// $TOKEN=$_SESSION["oauth_token"];
+// $TOKEN_SECRET=$_SESSION["oauth_token_secret"];
 $buscar=$_REQUEST['q'];
 // $buscar="#love";
 
