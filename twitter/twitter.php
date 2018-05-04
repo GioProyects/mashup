c79b1baf8eea8c2e34de9df9956b7ea6171fbea9
GioProyects <giogow3halo@gmail.com>
2018-05-04 08:32:50 -0500

    sdcdc

--- twitter/twitter.php
+++ twitter/twitter.php
@@ -6,11 +6,15 @@ define('CONSUMER_SECRET', 'hSe6ZQbao5wEyIvlGFXhA1itlSME9NBhsqOsiKYm5jmOUFJLMx');
 
 $TOKEN=$_SESSION["oauth_token"];
 $TOKEN_SECRET=$_SESSION["oauth_token_secret"];
+
+// $TOKEN='449924072-wLFORhlA94XcTTYJzWW9vnG4kN7TMBPkCAfgZKWi';
+// $TOKEN_SECRET='U5IAJAoLbzlzPLR98pHWyi137Y44S1LyV1F2LOYsWg8oi';
+
 //  echo $TOKEN;
 // echo "<br/>";
 //  echo $TOKEN_SECRET;
-$buscar=$_REQUEST['q'];
-// $buscar="amor";
+// $buscar=$_REQUEST['q'];
+$buscar="cat";
 
 if (isset($TOKEN)) {
   // $buscar="#".$buscar;
@@ -35,8 +39,7 @@ if (isset($TOKEN)) {
   foreach (json_decode($response)->statuses as $key) {
     $ids[]=$key->id;
   }
-
-  // var_dump($ids);
+  // var_dump($response);
   $json=array();
   $conta=0;
   foreach ($ids as $id) {
@@ -44,7 +47,6 @@ if (isset($TOKEN)) {
     $response2 = $twitter->setGetfield($getfield2)
         ->buildOauth($url2, $requestMethod)
         ->performRequest();
-
     $respuesta=json_decode($response2);
     if (!is_null($respuesta->geo)) {
       $json[]=array(
@@ -53,14 +55,24 @@ if (isset($TOKEN)) {
       );
     }
   }
-  var_dump($json);
-  // echo json_encode(array(
-  //   "datos"=>$json,
-  //   "tamanio"=>sizeof($json))
-  //   );
+
+  if (sizeof($json)>0) {
+    echo json_encode(array(
+      "datos"=>$json,
+      "tamanio"=>sizeof($json))
+      );
+  } else {
+    echo json_encode(array(
+      "datos"=>"No se encontraron coordenadas",
+      "tamanio"=>sizeof($json))
+      );
+  }
+
+  // var_dump($json);
+
 }else{
   echo json_encode(array(
-    "error"=>"no hay parametro"
+    "error"=>"no hay token"
   ));
 }
 
