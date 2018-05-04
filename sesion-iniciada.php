c25c121d10695d75ccdd5140221a7f9ed735f232
GioProyects <giogow3halo@gmail.com>
2018-05-03 22:37:08 -0500

    sdfsdf

--- sesion-iniciada.php
+++ sesion-iniciada.php
@@ -1,8 +1,30 @@
 <?php
 session_start();
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
-  // echo $_SESSION["oauth_token"];
-  // echo $_SESSION["oauth_token_secret"];
+  require_once 'TwitterAPIExchange.php';
+  define('CONSUMER_KEY', 'IsfOhNHmYtQS5myPZvXB7kqCf');
+  define('CONSUMER_SECRET', 'hSe6ZQbao5wEyIvlGFXhA1itlSME9NBhsqOsiKYm5jmOUFJLMx');
+  $TOKEN=$_SESSION["oauth_token"];
+  $TOKEN_SECRET=$_SESSION["oauth_token_secret"];
+
+  $url="https://api.twitter.com/oauth/access_token";
+  $requestMethod="POST";
+  $postfields=array(
+    "oauth_verifier"=>$_REQUEST["oauth_verifier"]
+  );
+  $settings = array(
+    'oauth_access_token' => $TOKEN,
+    'oauth_access_token_secret' => $TOKEN_SECRET,
+    'consumer_key' => CONSUMER_KEY,
+    'consumer_secret' => CONSUMER_SECRET
+  );
+
+  $twitter=new TwitterAPIExchange($settings);
+  $response3=$twitter->buildOauth($url,$requestMethod)
+                    ->setPostfields($postfields)
+                    ->performRequest();
+
+  var_dump($response3);
 }else {
   //echo "<p> Esta pagina es solo para usuarios registrados</p>";
   echo '<div class="alert alert-warning" role="alert">Necesita iniciar sesion para ver esta pagina</div>';
