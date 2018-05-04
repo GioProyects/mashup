a76b2d1044666ae9ec13f33b1ee0939456c4b3b9
GioProyects <giogow3halo@gmail.com>
2018-05-04 10:25:35 -0500

    final

--- index.php
+++ index.php
@@ -19,46 +19,75 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
   <link rel="stylesheet" href="css/estilos.css">
+
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<body>
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-faded" style="background:coral;">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
 
-      <!-- Brand -->
-      <a class="navbar-brand" href="#">Mashup</a>
-
-      <!-- Links -->
-      <div class="collapse navbar-collapse justify-content-end" id="nav-content">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="twitter/autenticacion.php">Iniciar Sesión</a>
-          </li>
-        </ul>
-      </nav>
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+  <div class="w3-container">
+    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+      <i class="fa fa-remove"></i>
+    </a>
+    <!--  agregar imagen-->
+    <img style="width:45%;" class="w3-round"><br><br>
+    <h4><b>MASHUP</b></h4>
+    <p class="w3-text-grey"></p>
   </div>
+  <div class="w3-bar-block">
+    <a href="twitter/autenticacion.php" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-twitter w3-hover-opacity w3-margin-right"></i>Iniciar Sesion</a>
+  </div>
+</nav>
 
-  <!-- <div class="container">
-    <nav class="navbar navbar-default">
-      <div class="navbar-header">
-        <a class="navbar-brand"> Youtube API + Google Maps API + Twitter API</a>
-      </div>
-      <ul class="nav navbar-nav navbar-right">
-        <div style="font-size:2em; color:DodgerBlue">
-          <i class="fab fa-twitter"></i>
-          <a href="twitter/autenticacion.php">Iniciar sesión</a>
-        </div>
-      </ul>
-    </nav>
-  </div> -->
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Header -->
+  <header id="portfolio">
+    <!--  Agregar imagen-->
+    <a href="#"><img style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+    <h1><b>Yotube API + Google Maps API + Twitter API</b></h1>
+    <div class="w3-section w3-bottombar w3-padding-16">
+
+    </div>
+    </div>
+  </header>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-dark-grey">
+
+  </footer>
+
+  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></div>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- sesion-iniciada.php
+++ sesion-iniciada.php
@@ -34,10 +34,6 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
   $_SESSION["oauth_token"]=$credenciales["oauth_token"];
   $_SESSION["oauth_token_secret"]=$credenciales["oauth_token_secret"];
 
-
-  //"oauth_token=449924072-wLFORhlA94XcTTYJzWW9vnG4kN7TMBPkCAfgZKWi&
-  //oauth_token_secret=U5IAJAoLbzlzPLR98pHWyi137Y44S1LyV1F2LOYsWg8oi&
-  //user_id=449924072&screen_name=giogow3halo"
 }else {
   //echo "<p> Esta pagina es solo para usuarios registrados</p>";
   echo '<div class="alert alert-warning" role="alert">Necesita iniciar sesion para ver esta pagina</div>';
@@ -57,30 +53,20 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
      <link rel="stylesheet" href="css/estilos.css">
+     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    </head>
-   <body>
+   <body class="w3-light-grey w3-content" style="max-width:1600px">
 
-     <div class="container">
-       <!-- <nav class="navbar navbar-expand-sm navbar-dark bg-faded">
-         <div class="navbar-header">
-           <a class="navbar-brand"> Youtube API + Google Maps API + Twitter API</a>
-         </div>
-         <ul class="nav navbar-nav navbar-right">
-           <div style="font-size:2em; color:DodgerBlue">
-             <i class="fab fa-twitter"></i>
-             <a href="cerrar-sesion.php">Cerrar sesión</a>
-           </div>
-         </ul>
-       </nav> -->
-
+     <!--<div class="container">
        <nav class="navbar navbar-expand-sm navbar-dark bg-faded" style="background:coral;">
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
            <span class="navbar-toggler-icon"></span>
          </button>
 
-         <!-- Brand -->
          <a class="navbar-brand" href="#">Youtube API + Google Maps API + Twitter API</a>
-         <!-- Links -->
+
          <div class="collapse navbar-collapse justify-content-end" id="nav-content">
            <ul class="navbar-nav">
              <li class="nav-item">
@@ -96,7 +82,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
 
         </div>
        <div class="row">
-         <!--<div class="col-md-12 col-md-12 col-lg-12">
+         <div class="col-md-12 col-md-12 col-lg-12">
            <form>
              <div class="form-group">
                <label for="usr">Nombre:</label>
@@ -114,7 +100,7 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
              <span class="input-group-text">Nombre:</span>
            </div>
            <input type="text" class="form-control" id="nomVideo" placeholder="Ejem:love,we,car" aria-label="Amount (rounded to the nearest dollar)" aria-describedby="basic-addon">
-         </div> -->
+         </div>
 
         <div class="input-group mb-3">
           <div class="input-group-prepend">
@@ -160,7 +146,117 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true) {
            <div id="map"></div>
          </div>
        </div>
+     </div>-->
+
+
+
+     <!-- Sidebar/menu -->
+     <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+       <div class="w3-container">
+         <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+           <i class="fa fa-remove"></i>
+         </a>
+         <!--  agregar imagen-->
+         <img id="fotoPerfil" style="width:45%;" class="w3-round"><br><br>
+         <h4><b>MASHUP</b></h4>
+         <p class="w3-text-grey"></p>
+       </div>
+       <div class="w3-bar-block">
+         <a href="twitter/autenticacion.php" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-twitter w3-hover-opacity w3-margin-right"></i>Cerrar Sesion</a>
+       </div>
+     </nav>
+
+     <!-- Overlay effect when opening sidebar on small screens -->
+     <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+     <!-- !PAGE CONTENT! -->
+     <div class="w3-main" style="margin-left:300px">
+
+       <!-- Header -->
+       <header id="portfolio">
+         <!--  Agregar imagen-->
+         <a href="#"><img id="fotoPerfil" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+         <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+         <div class="w3-container">
+         <h1><b>Yotube API + Google Maps API + Twitter API</b></h1>
+         <div class="w3-section w3-bottombar w3-padding-16">
+
+         </div>
+         </div>
+       </header>
+
+       <!-- First Photo Grid-->
+       <div class="w3-row-padding">
+         <div class="w3-third w3-container w3-margin-bottom">
+           <div class="input-group mb-3">
+             <div class="input-group-prepend">
+               <span class="input-group-text">Nombre:</span>
+             </div>
+             <input id="nomVideo" type="text" class="form-control" placeholder="Ejemplos:love,we,car" aria-label="Amount (rounded to the nearest dollar)" aria-describedby="basic-addon">
+           </div>
+         </div>
+
+         <div class="w3-third w3-container w3-margin-bottom">
+           <div class="input-group mb-3">
+             <div class="input-group-prepend">
+               <span class="input-group-text">Numero</span>
+             </div>
+             <input id="numVideo" type="text" class="form-control" placeholder="de 100 en adelante" aria-label="Amount (rounded to the nearest dollar)" aria-describedby="basic-addon">
+           </div>
+         </div>
+
+         <div class="w3-third w3-container">
+           <button id="btnBuscar" type="button" class="btn btn-info">Buscar</button>
+         </div>
+       </div>
+
+       <!-- Images of Me -->
+        <div class="w3-row-padding w3-padding-16" id="about">
+          <div class="w3-col m6">
+            <div class="row" id="results">
+            </div>
+            <div class="row">
+              <div class="w3-col m3">
+              </div>
+              <div class="w3-col m3">
+                <div id="bloque_paginacion" style="display:none;">
+                  <ul class="pagination">
+                    <li id="btn_prev" class="page-item"><a class="page-link" >Anterior</a></li>
+                    <li id="btn_next" class="page-item"><a class="page-link" >Siguiente</a></li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w3-col m6">
+            <div id="map">
+
+            </div>
+          </div>
+        </div>
+
+       <!-- final pagina -->
+       <div class="w3-black w3-center w3-padding-24">Version 1.0</div>
+
+     <!-- End page content -->
      </div>
+
+     <script>
+     // Script to open and close sidebar
+     function w3_open() {
+         document.getElementById("mySidebar").style.display = "block";
+         document.getElementById("myOverlay").style.display = "block";
+     }
+
+     function w3_close() {
+         document.getElementById("mySidebar").style.display = "none";
+         document.getElementById("myOverlay").style.display = "none";
+     }
+     </script>
+     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 
      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
